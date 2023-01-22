b4495202a93eb248b1fa42f1b961d4c3a2bfac4b
jyjdal <1604070358@qq.com>
2023-01-22 22:11:58 +0800

    Decode I-Type instructions.

--- src/instruction.rs
+++ src/instruction.rs
@@ -3,13 +3,32 @@ use crate::register::Register;
 #[derive(Debug)]
 pub enum Instruction {
     Undefined,
-    // Opcode: 0b0010011
+
     Addi { rd: Register, rs1: Register, imm: i32 },
     Slti { rd: Register, rs1: Register, imm: i32 },
     Sltiu { rd: Register, rs1: Register, imm: i32 },
     Xori { rd: Register, rs1: Register, imm: i32 },
     Ori { rd: Register, rs1: Register, imm: i32 },
     Andi { rd: Register, rs1: Register, imm: i32 },
+
+    Slli { rd: Register, rs1: Register, shamt: u32 },
+    Srli { rd: Register, rs1: Register, shamt: u32 },
+    Srai { rd: Register, rs1: Register, shamt: u32 },
+
+    Lb { rd: Register, rs1: Register, imm: i32 },
+    Lh { rd: Register, rs1: Register, imm: i32 },
+    Lw { rd: Register, rs1: Register, imm: i32 },
+    Lbu { rd: Register, rs1: Register, imm: i32 },
+    Lhu { rd: Register, rs1: Register, imm: i32 },
+    Lwu { rd: Register, rs1: Register, imm: i32 },
+    Ld { rd: Register, rs1: Register, imm: i32 },
+
+    Fence { rd: Register, rs1: Register, imm: i32 },
+
+    Jalr { rd: Register, rs1: Register, imm: i32 },
+
+    Ecall,
+    Ebreak,
 }
 
 // Instruction type, see specification chapter 27: RV32/64G Instruction Set Listings
@@ -36,41 +55,102 @@ impl InstType {
 
         match self {
             InstType::I => {
-                // Decode base type fields
-                let imm = (inst >> 20) & 0b1111_1111_1111;
-                let rs1: Register = (((inst >> 15) & 0b1111_1) as usize).into();
-                let func3 = (inst >> 12) & 0b111;
-                let rd: Register = (((inst >> 7) & 0b1111_1) as usize).into();
-
-                // Sign extend the immediate
-                let imm = ((imm as i32) << 20) >> 20;
-
-                return match opcode {
-                    0b0010011 => {
-                        match func3 {
-                            0b000 => Instruction::Addi { rd, rs1, imm },
-                            0b010 => Instruction::Slti { rd, rs1, imm },
-                            0b011 => Instruction::Sltiu { rd, rs1, imm },
-                            0b100 => Instruction::Xori { rd, rs1, imm },
-                            0b110 => Instruction::Ori { rd, rs1, imm },
-                            0b111 => Instruction::Andi { rd, rs1, imm },
-                            _ => Instruction::Undefined
-                        }
-                    }
-                    _ => { Instruction::Undefined }
-                };
+                return InstType::decode_type_i(inst);
+            }
+            InstType::R => {
+                return InstType::decode_type_r(inst);
+            }
+            InstType::S => {
+                return Instruction::Undefined;
             }
             _ => {}
         }
 
         return Instruction::Undefined;
     }
+
+    fn decode_type_i(inst: u32) -> Instruction {
+        // Get opcode
+        let opcode = inst & 0b1111111;
+
+        // Decode base type fields
+        let imm = (inst >> 20) & 0b1111_1111_1111;
+        let rs1: Register = (((inst >> 15) & 0b1111_1) as usize).into();
+        let func3 = (inst >> 12) & 0b111;
+        let rd: Register = (((inst >> 7) & 0b1111_1) as usize).into();
+
+        // Sign extend the immediate
+        let imm = ((imm as i32) << 20) >> 20;
+
+        return match opcode {
+            0b0000011 => {
+                match func3 {
+                    0b000 => Instruction::Lb { rd, rs1, imm },
+                    0b001 => Instruction::Lh { rd, rs1, imm },
+                    0b010 => Instruction::Lw { rd, rs1, imm },
+                    0b100 => Instruction::Lbu { rd, rs1, imm },
+                    0b101 => Instruction::Lhu { rd, rs1, imm },
+                    0b110 => Instruction::Lwu { rd, rs1, imm },
+                    0b011 => Instruction::Ld { rd, rs1, imm },
+                    _ => Instruction::Undefined
+                }
+            }
+            0b0010011 => {
+                // Extract shift fields
+                let shamt = (imm & 0b111_111) as u32;
+                let shiftop = (imm >> 6) & 0b111_111;
+
+                match func3 {
+                    0b000 => Instruction::Addi { rd, rs1, imm },
+                    0b010 => Instruction::Slti { rd, rs1, imm },
+                    0b011 => Instruction::Sltiu { rd, rs1, imm },
+                    0b100 => Instruction::Xori { rd, rs1, imm },
+                    0b110 => Instruction::Ori { rd, rs1, imm },
+                    0b111 => Instruction::Andi { rd, rs1, imm },
+                    0b001 => Instruction::Slli { rd, rs1, shamt },
+                    0b101 if shiftop == 0 => Instruction::Srli { rd, rs1, shamt },
+                    0b101 if shiftop == 0b010_000 => Instruction::Srai { rd, rs1, shamt },
+                    _ => Instruction::Undefined
+                }
+            }
+            0b0001111 => {
+                match func3 {
+                    0b000 => Instruction::Fence { rd, rs1, imm },
+                    _ => Instruction::Undefined
+                }
+            }
+            0b1100111 => {
+                match func3 {
+                    0b000 => Instruction::Jalr { rd, rs1, imm },
+                    _ => Instruction::Undefined
+                }
+            }
+            0b1110011 => {
+                match func3 {
+                    0b000 if imm == 0 && rs1 == Register::X0
+                        && rd == Register::X0 => Instruction::Ecall,
+                    0b000 if imm == 1 && rs1 == Register::X0
+                        && rd == Register::X0 => Instruction::Ebreak,
+                    _ => Instruction::Undefined
+                }
+            }
+            _ => { Instruction::Undefined }
+        };
+    }
+
+    fn decode_type_r(inst: u32) -> Instruction {
+        unimplemented!()
+    }
+
+    fn decode_type_u(inst: u32) -> Instruction {
+        unimplemented!()
+    }
 }
 
 #[test]
 fn test() {
     // Instruction 0x00000013 is Addi { rd: X0, rs1: X0, imm: 0 }.
-    decode(19);
+    decode(0x000000e7);
     panic!("Failed")
 }
 
@@ -78,7 +158,7 @@ fn test() {
 fn decode(inst: u32) {
     let opcode = inst & 0b1111111;
 
-    let decoded = if let Some(typ) = OPCODE_TYPE[opcode as usize] {
+    let decoded = if let Some(typ) = &OPCODE_TYPE[opcode as usize] {
         typ.decode(inst)
     } else {
         Instruction::Undefined
@@ -92,7 +172,7 @@ const OPCODE_TYPE: [Option<InstType>; 128] = [
     /* 0b0000000 */ None,
     /* 0b0000001 */ None,
     /* 0b0000010 */ None,
-    /* 0b0000011 */ None,
+    /* 0b0000011 */ Some(InstType::I),
     /* 0b0000100 */ None,
     /* 0b0000101 */ None,
     /* 0b0000110 */ None,
@@ -104,7 +184,7 @@ const OPCODE_TYPE: [Option<InstType>; 128] = [
     /* 0b0001100 */ None,
     /* 0b0001101 */ None,
     /* 0b0001110 */ None,
-    /* 0b0001111 */ None,
+    /* 0b0001111 */ Some(InstType::I),
     /* 0b0010000 */ None,
     /* 0b0010001 */ None,
     /* 0b0010010 */ None,
@@ -112,7 +192,7 @@ const OPCODE_TYPE: [Option<InstType>; 128] = [
     /* 0b0010100 */ None,
     /* 0b0010101 */ None,
     /* 0b0010110 */ None,
-    /* 0b0010111 */ None,
+    /* 0b0010111 */ Some(InstType::U),
     /* 0b0011000 */ None,
     /* 0b0011001 */ None,
     /* 0b0011010 */ None,
@@ -124,7 +204,7 @@ const OPCODE_TYPE: [Option<InstType>; 128] = [
     /* 0b0100000 */ None,
     /* 0b0100001 */ None,
     /* 0b0100010 */ None,
-    /* 0b0100011 */ None,
+    /* 0b0100011 */ Some(InstType::S),
     /* 0b0100100 */ None,
     /* 0b0100101 */ None,
     /* 0b0100110 */ None,
@@ -140,11 +220,11 @@ const OPCODE_TYPE: [Option<InstType>; 128] = [
     /* 0b0110000 */ None,
     /* 0b0110001 */ None,
     /* 0b0110010 */ None,
-    /* 0b0110011 */ None,
+    /* 0b0110011 */ Some(InstType::R),
     /* 0b0110100 */ None,
     /* 0b0110101 */ None,
     /* 0b0110110 */ None,
-    /* 0b0110111 */ None,
+    /* 0b0110111 */ Some(InstType::U),
     /* 0b0111000 */ None,
     /* 0b0111001 */ None,
     /* 0b0111010 */ None,
@@ -188,11 +268,11 @@ const OPCODE_TYPE: [Option<InstType>; 128] = [
     /* 0b1100000 */ None,
     /* 0b1100001 */ None,
     /* 0b1100010 */ None,
-    /* 0b1100011 */ None,
+    /* 0b1100011 */ Some(InstType::B),
     /* 0b1100100 */ None,
     /* 0b1100101 */ None,
     /* 0b1100110 */ None,
-    /* 0b1100111 */ None,
+    /* 0b1100111 */ Some(InstType::I),
     /* 0b1101000 */ None,
     /* 0b1101001 */ None,
     /* 0b1101010 */ None,
@@ -200,11 +280,11 @@ const OPCODE_TYPE: [Option<InstType>; 128] = [
     /* 0b1101100 */ None,
     /* 0b1101101 */ None,
     /* 0b1101110 */ None,
-    /* 0b1101111 */ None,
+    /* 0b1101111 */ Some(InstType::J),
     /* 0b1110000 */ None,
     /* 0b1110001 */ None,
     /* 0b1110010 */ None,
-    /* 0b1110011 */ None,
+    /* 0b1110011 */ Some(InstType::I),
     /* 0b1110100 */ None,
     /* 0b1110101 */ None,
     /* 0b1110110 */ None,
